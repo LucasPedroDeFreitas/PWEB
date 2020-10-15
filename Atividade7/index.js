26f4114edd86fdff62d765bfcd811bec64c78dd3
LucasPedroDeFreitas <69942073+LucasPedroDeFreitas@users.noreply.github.com>
2020-10-14 22:16:55 -0300

    Add files via upload

---
+++ Atividade7/index.js
@@ -0,0 +1,13 @@
+var Aluno1 = {
+  ra: '0030481823000',
+  nome: 'José de Sousa'
+}
+console.log(Aluno1);
+
+Aluno1.ra = '0030480999';
+Aluno1.nome = 'Ana Paula';
+console.log(Aluno1);
+
+Aluno1['ra'] = '0030481654';
+Aluno1['nome'] = 'Luis de Andrade';
+console.log(Aluno1);
